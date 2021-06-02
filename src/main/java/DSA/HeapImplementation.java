93c53b65cfbea932a3dfed6a6b2fa68580fe877f
Ashok <yashok42@gmail.com>
2021-06-02 10:22:29 +0530

    implemented add functionality into heap and also heapfyup logic

--- src/main/java/DSA/HeapImplementation.java
+++ src/main/java/DSA/HeapImplementation.java
@@ -1,8 +1,21 @@
 package DSA;
 
+import DSA.heap.MinHeap;
+
 public class HeapImplementation {
 
     public static void main(String[] args) {
+        MinHeap minHeap = new MinHeap(10);
+        minHeap.add(60);
+        minHeap.add(20);
+        minHeap.add(40);
+        minHeap.add(42);
+        minHeap.add(45);
+        minHeap.add(21);
+        minHeap.add(22);
+        minHeap.add(5);
+        minHeap.add(4);
 
+        minHeap.printHeap();
     }
 }
--- src/main/java/DSA/heap/MinHeap.java
+++ src/main/java/DSA/heap/MinHeap.java
@@ -1,11 +1,18 @@
 package DSA.heap;
 
+import java.util.Arrays;
+
 public class MinHeap {
 
     private int capacity;
     private int size;
+    int [] items;
 
-    int [] items = new int[capacity];
+    public MinHeap(int capacity) {
+        this.capacity = capacity;
+        this.items = new int[capacity];
+    }
+
 
     public int getLeftChildIndex(int parentIndex) { return 2 * parentIndex + 1; }
     public int getRightChildIndex(int parentIndex) {return 2 * parentIndex + 2; }
@@ -13,10 +20,34 @@ public class MinHeap {
 
     public boolean hasLeftChild(int parentIndex) { return getLeftChildIndex(parentIndex) < size; }
     public boolean hasRightChild(int parentIndex) { return getRightChildIndex(parentIndex) < size; }
-    public boolean hasParent(int childIndex) {return getParentIndex(childIndex) < size; }
+    public boolean hasParent(int childIndex) {return getParentIndex(childIndex) < size && getParentIndex(childIndex) >= 0; } //check this with root element
 
-    
+    public int getParent(int itemIndex) { return items[getParentIndex(itemIndex)]; };
+    public int getLeftChild(int itemIndex) { return items[getLeftChild(itemIndex)]; };
+    public int getRightChild(int itemIndex) { return items[getRightChild(itemIndex)]; };
 
+    private void swap(int index1, int index2){
+        int temp = items[index1];
+        items[index1] = items[index2];
+        items[index2] = temp;
+    }
 
+    public void add(int item) {
+        //insert item to items
+        items[size] = item;
+        size++;
+        heapfyUp();
+    }
 
+    private void heapfyUp(){
+        int index = size - 1;
+        while(hasParent(index) && getParent(index) > items[index]) {
+            swap(getParentIndex(index), index);
+            index = getParentIndex(index);
+        }
+    }
+
+    public void printHeap() {
+        System.out.println(Arrays.toString(items));
+    }
 }
