8043f5c799372e3639130af343c6268e50b378d4
yAshok8 <ashokb.yadav42@gmail.com>
2021-08-27 21:12:35 +0530

    Finalized the Median of 2 sorted array code

--- src/main/java/DSA/TwoSortedArraysMedian.java
+++ src/main/java/DSA/TwoSortedArraysMedian.java
@@ -5,18 +5,18 @@ public class TwoSortedArraysMedian {
 //        int [] x = {3,5,13,18,21,23};
 //        int [] y = {8,9,15,19,35,40};
 
-        int [] x = {1,2,3,4,5,6};
-        int [] y = {};
+        int [] x = {1};
+        int [] y = {1};
         double median = findMedian(x,y);
         System.out.println("\n Median: "+median);
     }
 
-    private static double findMedian(int[] inputArr1, int[] inputArr2) {
-        int x = inputArr1.length;
-        int y = inputArr2.length;
+    private static double findMedian(int[] nums1, int[] nums2) {
+        int x = nums1.length;
+        int y = nums2.length;
 
         if(x>y)
-            return findMedian(inputArr2, inputArr1);
+            return findMedian(nums2, nums1);
 
         int low = 0;
         int high = x;
@@ -25,11 +25,11 @@ public class TwoSortedArraysMedian {
             int partitionX = (low+high) / 2;
             int partitionY = (x+y+1) / 2 - partitionX;
 
-            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : inputArr1[partitionX-1];
-            int minRightX = (partitionX == x) ? Integer.MAX_VALUE : inputArr1[partitionX];
+            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX-1];
+            int minRightX = (partitionX == x) ? Integer.MAX_VALUE : nums1[partitionX];
 
-            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : inputArr2[partitionY-1];
-            int minRightY = (partitionY == y) ? Integer.MIN_VALUE : inputArr2[partitionY];
+            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY-1];
+            int minRightY = (partitionY == y) ? Integer.MAX_VALUE : nums2[partitionY];
 
             if(maxLeftX <= minRightY && minRightX >= maxLeftY){
                 if((x+y) % 2 == 0){
