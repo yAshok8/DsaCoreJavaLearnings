51214f7881dad148a4c9b519fe21e0b61f09033b
yAshok8 <ashokb.yadav42@gmail.com>
2021-09-02 21:17:59 +0530

    Finalized code Note: Yet to be submitted to leetcode

--- src/main/java/DSA/strings/LongestPalindromicSubstringOptimized.java
+++ src/main/java/DSA/strings/LongestPalindromicSubstringOptimized.java
@@ -2,31 +2,50 @@ package DSA.strings;
 
 public class LongestPalindromicSubstringOptimized {
     public static void main(String[] args) {
-        String s = "ashok";
+        String s = "valdenedlakhuva";
         System.out.println(longestPalindromicSubstring(s));
-    }
-    private static String longestPalindromicSubstring(String s){
-        int size = s.length();
-        int [][] dynamicPalindromeMap = new int [size][size];
-        for(int i=0; i<size; i++){
+/*        int size = s.length()-1;
+        for(int i=0; i<=size; i++) {
             int innerI = i;
-            for(int j=0; j<=size-i; j++){
-                int left = j;
-                int right = innerI++;
-                if(i==0){
-                    dynamicPalindromeMap[left][right] = 1;
-                }else {
-                    boolean b = s.charAt(left) == s.charAt(right);
-                    if(i==1){
-                        dynamicPalindromeMap[left][right] = b ? 1 : 0;
-                    }else{
-                        dynamicPalindromeMap[left][right] = (b && dynamicPalindromeMap[left+1][right-1] == 1 ) ? 1 : 0;
-                    }
-                }
-                System.out.println(dynamicPalindromeMap[left][right]);
+            for (int j = 0; j <= size - i; j++) {
+                System.out.print("("+j+","+(innerI++)+") ");
             }
             System.out.println();
+        }*/
+
+   }
+    private static String longestPalindromicSubstring(String s){
+        int size = s.length();
+        if(size==1)
+            return s;
+        int [][] dynamicPalindromeMap = new int [size][size];
+        int maxLen = 0;
+        int tempMax = 0;
+        String longestString = "";
+        for(int i=0; i<=size-1; i++){
+            int innerI = i;
+            for(int j=0; j<=size-1-i; j++){
+                int right = innerI++;
+                if(i==0){
+                    dynamicPalindromeMap[j][right] = 1;
+                }else {
+                    boolean b = s.charAt(j) == s.charAt(right);
+                    if(i==1){
+                        dynamicPalindromeMap[j][right] = b ? 1 : 0;
+                    }else{
+                        dynamicPalindromeMap[j][right] = (b && dynamicPalindromeMap[j+1][right-1] == 1 ) ? 1 : 0;
+                    }
+                    //here check j and right diff
+                    if(dynamicPalindromeMap[j][right] == 1){
+                        tempMax = right - j;
+                        if(tempMax > maxLen){
+                            longestString = s.substring(j, right+1);
+                            maxLen = tempMax;
+                        }
+                    }
+                }
+            }
         }
-        return null;
+        return longestString;
     }
 }
