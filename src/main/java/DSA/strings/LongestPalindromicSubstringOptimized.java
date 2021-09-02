e93efc2c1d7bbd730e177095972af383549d0669
yAshok8 <ashokb.yadav42@gmail.com>
2021-09-03 00:13:59 +0530

    Final Optimized code for LongestPalindromicSubstring. Leet code URL: https://leetcode.com/problems/longest-palindromic-substring/

--- src/main/java/DSA/strings/LongestPalindromicSubstringOptimized.java
+++ src/main/java/DSA/strings/LongestPalindromicSubstringOptimized.java
@@ -2,17 +2,8 @@ package DSA.strings;
 
 public class LongestPalindromicSubstringOptimized {
     public static void main(String[] args) {
-        String s = "valdenedlakhuva";
+        String s = "dfdk";
         System.out.println(longestPalindromicSubstring(s));
-/*        int size = s.length()-1;
-        for(int i=0; i<=size; i++) {
-            int innerI = i;
-            for (int j = 0; j <= size - i; j++) {
-                System.out.print("("+j+","+(innerI++)+") ");
-            }
-            System.out.println();
-        }*/
-
    }
     private static String longestPalindromicSubstring(String s){
         int size = s.length();
@@ -29,19 +20,19 @@ public class LongestPalindromicSubstringOptimized {
                 if(i==0){
                     dynamicPalindromeMap[j][right] = 1;
                 }else {
-                    boolean b = s.charAt(j) == s.charAt(right);
+                    boolean edgeCharsSame = s.charAt(j) == s.charAt(right);
                     if(i==1){
-                        dynamicPalindromeMap[j][right] = b ? 1 : 0;
+                        dynamicPalindromeMap[j][right] = edgeCharsSame ? 1 : 0;
                     }else{
-                        dynamicPalindromeMap[j][right] = (b && dynamicPalindromeMap[j+1][right-1] == 1 ) ? 1 : 0;
+                        dynamicPalindromeMap[j][right] = (edgeCharsSame && dynamicPalindromeMap[j+1][right-1] == 1 ) ? 1 : 0;
                     }
-                    //here check j and right diff
-                    if(dynamicPalindromeMap[j][right] == 1){
-                        tempMax = right - j;
-                        if(tempMax > maxLen){
-                            longestString = s.substring(j, right+1);
-                            maxLen = tempMax;
-                        }
+                }
+                //here check j and right diff
+                if(dynamicPalindromeMap[j][right] == 1){
+                    tempMax = right - j + 1;
+                    if(tempMax > maxLen){
+                        longestString = s.substring(j, right+1);
+                        maxLen = tempMax;
                     }
                 }
             }
