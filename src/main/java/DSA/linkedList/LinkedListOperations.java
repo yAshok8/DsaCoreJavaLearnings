c39a10f0ad548067dc33bff9607262e9784f84f6
yAshok8 <ashokb.yadav42@gmail.com>
2021-09-07 11:01:36 +0530

    Added code for merging 2 sorted Linked List using recursion approach LeetCode URL : https://leetcode.com/problems/merge-two-sorted-lists/

---
+++ src/main/java/DSA/linkedList/LinkedListOperations.java
@@ -0,0 +1,29 @@
+package DSA.linkedList;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class LinkedListOperations {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null)
+            return l2;
+        if(l2 == null)
+            return l1;
+        ListNode result = null;
+        if(l1.val < l2.val){
+            result = l1;
+            result.next = mergeTwoLists(l1.next, l2);
+        }else{
+            result = l2;
+            result.next = mergeTwoLists(l1, l2.next);
+        }
+        return result;
+    }
+}
