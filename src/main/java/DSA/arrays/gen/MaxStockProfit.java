3edbc4521456ab4dc50cf814760afa90e4c0fe40
yAshok8 <ashokb.yadav42@gmail.com>
2021-09-12 13:26:38 +0530

    Added Max Stock Profit-I code Leet Code URL: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/

---
+++ src/main/java/DSA/arrays/gen/MaxStockProfit.java
@@ -0,0 +1,23 @@
+package DSA.arrays.gen;
+
+class MaxStockProfit {
+    public static void main(String[] args) {
+        int [] prices = {7,1,6,9,1,3,8};
+//        int [] prices = {7,1,5,3,6,4};
+//        int [] prices = {7,6,4,3,1};
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1)
+            return 0;
+        int maxP = 0;
+        int minPrice = prices[0];
+        for (int i=0; i<prices.length; i++){
+            maxP = Math.max(maxP, prices[i] - minPrice);
+            minPrice = Math.min(minPrice, prices[i]);
+        }
+        return maxP;
+    }
+
+}
