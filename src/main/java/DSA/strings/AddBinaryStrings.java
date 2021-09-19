8555a003a913170cf06d16c5ec179b707dbbf7ad
Ashok <yashok42@gmail.com>
2021-09-19 16:16:17 +0530

    Added Simple Clean Code for AddBinaryStrings Leetcode URL: https://leetcode.com/problems/add-binary/

--- src/main/java/DSA/strings/AddBinaryStrings.java
+++ src/main/java/DSA/strings/AddBinaryStrings.java
@@ -3,15 +3,43 @@ package DSA.strings;
 class AddBinaryStrings {
 
     public static void main(String[] args) {
-        String a = "1010";
-        String b = "1011";
-        System.out.println(addBinary(a, b));
+        //        String a = "1010";
+        //        String b = "1011";
+        String a = "100";
+        String b = "110010";
+        System.out.println(addBinaryBF(a, b));
+        System.out.println(addBinaryCleanOptimized(a, b));
     }
 
-    private static String addBinary(String a, String b) {
+    private static String addBinaryCleanOptimized(String a, String b){
+        int a_idx = a.length() - 1;
+        int b_idx = b.length() - 1;
+        int carry = 0;
+        StringBuilder sbr = new StringBuilder();
+
+        while(a_idx > -1 || b_idx > -1 || carry == 1){
+
+            if(a_idx > -1)
+                carry += a.charAt(a_idx) - '0';
+
+            if(b_idx > -1)
+                carry += b.charAt(b_idx) - '0';
+
+            sbr.append(carry%2);
+
+            carry = carry / 2;
+            a_idx--;
+            b_idx--;
+
+        }
+
+        return sbr.reverse().toString();
+    }
+
+    private static String addBinaryBF(String a, String b) {
         int a_index = a.length()-1;
         int b_index = b.length()-1;
-        StringBuffer ans = new StringBuffer();
+        StringBuilder ans = new StringBuilder();
         int carry = 0;
         while(a_index >= 0 && b_index >= 0){
             int a_c = Math.abs(a.charAt(a_index)-48);
@@ -46,9 +74,41 @@ class AddBinaryStrings {
         }
         if(a_index<0){
             //while loop for b_index to be 0
+            while(b_index >= 0){
+                if(carry == 0){
+                    ans.append(Math.abs(b.charAt(b_index)-48));
+                }else{
+                    if(Math.abs(b.charAt(b_index)-48) == 1){
+                        ans.append(0);
+                        carry = 1;
+                    }else{
+                        ans.append(1);
+                        carry = 0;
+                    }
+                }
+                b_index--;
+            }
         }else{
             //while loop for a_index to be 0
+            while(a_index >= 0){
+                if(carry == 0){
+                    ans.append(Math.abs(a.charAt(a_index)-48));
+                }else{
+                    if(Math.abs(a.charAt(a_index)-48) == 1){
+                        ans.append(0);
+                        carry = 1;
+                    }else{
+                        ans.append(1);
+                        carry = 0;
+                    }
+                }
+                a_index--;
+            }
         }
+
+        if(carry==1)
+            ans.append(1);
+
         return ans.reverse().toString();
     }
 }
