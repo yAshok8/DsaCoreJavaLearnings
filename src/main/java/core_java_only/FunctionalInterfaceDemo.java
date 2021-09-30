43bbaaf35d64785c176084a1efbd4c9d32866e85
yAshok8 <ashokb.yadav42@gmail.com>
2021-09-30 12:12:41 +0530

    BinaryOperator Interface Implementation

--- src/main/java/core_java_only/FunctionalInterfaceDemo.java
+++ src/main/java/core_java_only/FunctionalInterfaceDemo.java
@@ -1,9 +1,26 @@
 package core_java_only;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
 public class FunctionalInterfaceDemo {
     public static void main(String[] args) {
         Square s = (int i) -> i*i;
         int ans = s.calculate(10);
         System.out.println(ans);
+
+        //Binary Operator Demo
+        //Takes 2 arguments of a type and returns the same type value
+        BinaryOperator<List<String>> commonNames = (nameList1, nameList2) -> {
+            nameList1.retainAll(nameList2);
+            return nameList1;
+        };
+        List<String> homeFriends = new ArrayList<>(Arrays.asList("Mukesh", "Jayesh", "Vinod", "Meghana"));
+        List<String> officeFriends = new ArrayList<>(Arrays.asList("Meghana", "Kapil", "Nishant", "Mukesh"));
+        System.out.println(commonNames.apply(homeFriends, officeFriends));
+
+
     }
 }
