bf8920ac2926f363c880f375e8e019703b119b4b
Ashok <yashok42@gmail.com>
2021-11-03 17:10:42 +0530

    Finding out the common elements between 2 arrays or in other words the IntersectionOfTwoArrays Leet URL: https://leetcode.com/problems/intersection-of-two-arrays-ii/

---
+++ src/main/java/DSA/arrays/two_pointers/IntersectionOfTwoArrays.java
@@ -0,0 +1,43 @@
+package DSA.arrays.two_pointers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class IntersectionOfTwoArrays {
+    public static void main(String[] args) {
+        int [] num1 = {9,4,9,8,4};
+        int [] num2 = {4,9,5};
+
+        int [] res = intersect(num1, num2);
+        for(int elem: res)
+            System.out.print(elem + " ");
+
+    }
+
+    private static int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length)
+            return intersect(nums2, nums1);
+
+        Map<Integer, Integer> elemsCount = new HashMap<>();
+        for (int elem: nums1)
+            elemsCount.put(elem, elemsCount.getOrDefault(elem, 0) + 1);
+
+        List<Integer> intersectionElems = new ArrayList<>();
+        for (int elem: nums2){
+            if (elemsCount.containsKey(elem) && elemsCount.get(elem) > 0){
+                intersectionElems.add(elem);
+                elemsCount.put(elem, elemsCount.get(elem)-1);
+            }
+        }
+
+        int [] resArr = new int[intersectionElems.size()];
+        int k = 0;
+        for (int elem: intersectionElems){
+            resArr[k++] = elem;
+        }
+
+        return resArr;
+    }
+}
