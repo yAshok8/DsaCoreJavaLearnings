3984ca9e08186e46a82fe2307dcdb3e2edbb32a2
Ashok <yashok42@gmail.com>
2021-11-15 14:08:01 +0530

    Hacker-rank Wissen Code Challenge Week 1

--- src/main/java/hackerrank/week1/PositiveNegativeZeroRatios.java
+++ src/main/java/hackerrank/week1/PositiveNegativeZeroRatios.java
@@ -1,13 +1,7 @@
-package hackerrank;
+package hackerrank.week1;
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
@@ -20,7 +14,7 @@ Output Below:
 0.222222
 0.555556
 0.222222
-* */
+**/
 
 class Result {
 
