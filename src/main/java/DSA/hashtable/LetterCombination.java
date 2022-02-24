e51dd1ef17700103e2727105a89373faa534818c
ts-ashok.yadav <ts-ashok.yadav@rakuten.com>
2022-02-24 11:31:31 +0530

    added two leetcode problems in hashmap category

---
+++ src/main/java/DSA/hashtable/LetterCombination.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LetterCombination {
+    public static void main(String[] args) {
+        String digits = "789";
+        String [] map = new String[] {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        List<String> res = new ArrayList<>();
+        BT(digits, 0, map, "", res);
+        System.out.println(res);
+    }
+
+    public static void BT(String digits, int index, String [] map, String currentStr, List<String> res) {
+        if (index == digits.length()) {
+            res.add(currentStr);
+            return;
+        }
+
+        int thisDigit = Integer.parseInt(String.valueOf(digits.charAt(index)));
+        String [] splittedCharacters = map[thisDigit].split("");
+
+        index++;
+
+        for (String splittedCharacter: splittedCharacters) {
+            BT(digits, index, map, currentStr+splittedCharacter, res);
+        }
+
+    }
+}
---
+++ src/main/java/DSA/hashtable/RingsAndRods.java
@@ -0,0 +1,25 @@
+class RingsAndRods {
+
+    public static void main(String[] args) {
+//        String rings = "B0B6G0R6R0R6G9";
+        String rings = "B0R0G0R9R0B0G0";
+        int res = countPoints(rings);
+        System.out.println(res);
+    }
+
+    private static int countPoints(String rings) {
+        String [] rodsVal = new String[] {"", "", "", "", "", "", "", "", "", ""};
+        int index = 0;
+        int res = 0;
+        while (index < rings.length()) {
+            char color = rings.charAt(index);
+            int rod = rings.charAt(index+1) - 48;
+            if (rodsVal[rod] != null && rodsVal[rod].length() != 3 && !rodsVal[rod].contains(String.valueOf(color))) {
+                rodsVal[rod] += color;
+                if (rodsVal[rod].length() == 3) res++;
+            }
+            index+=2;
+        }
+        return res;
+    }
+}
