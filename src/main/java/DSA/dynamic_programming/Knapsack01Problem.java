48f0a251b0f6dd8b9aa2cb60c420ec2713d4de79
yAshok8 <ashokb.yadav42@gmail.com>
2022-02-28 20:34:21 +0530

    Added code for knapsack 0|1 problem

---
+++ src/main/java/DSA/dynamic_programming/Knapsack01Problem.java
@@ -0,0 +1,22 @@
+package DSA.dynamic_programming;
+
+public class Knapsack01Problem {
+
+    public static void main(String[] args) {
+        int [] wt = {1, 5, 3, 4, 8};
+        int [] val = {12, 3, 4, 7, 21};
+        System.out.println(KS(wt, val, 0, 12, wt.length - 1));
+    }
+
+    private static int KS(int [] wt, int [] val, int price, int W, int index) {
+        if (W == 0 || index == 0)
+            return price;
+
+        if (wt[index] < W) {
+            return Math.max(KS(wt, val, price, W-wt[index], index - 1),
+                    KS(wt, val, price + val[index], W, index - 1));
+        }
+
+        return KS(wt, val, price, W, index - 1);
+    }
+}
