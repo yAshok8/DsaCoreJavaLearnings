a3a727f7a03f77e08bf251ab959ad4fb523ca620
Ashok <yashok42@gmail.com>
2022-11-07 22:38:58 +0530

    code to search element in sorted and rotated array

--- src/main/java/DSA/arrays/SearchInRotatedSortedArray.java
+++ src/main/java/DSA/arrays/SearchInRotatedSortedArray.java
@@ -3,12 +3,13 @@ package DSA.arrays;
 public class SearchInRotatedSortedArray {
 
     public static void main(String[] args) {
-        int [] elems = new int[] {20, 22, 23, 45, 3, 9, 14, 18};
+//        int [] elems = new int[] {19, 20, 22, 23, 45, 3, 9, 14, 18};
+        int [] elems = new int[] { 9, 14, 18, 19, 20, 22, 23, 45, 3};
         int start = 0;
         int end = elems.length - 1;
         int size = elems.length;
-        int r = 4;
-        int targetNum = 20;
+        int r = 8;
+        int targetNum = 45;
         while (start <= end) {
             int mid = (start + end) / 2;
             int resIndex = (mid + r) % size;
