26c9ae748c0323088c428212536e090159f65701
Ashok <yashok42@gmail.com>
2022-11-14 00:03:25 +0530

    optimized the code

--- src/main/java/DSA/arrays/gen/SplitIntoSubArray.java
+++ src/main/java/DSA/arrays/gen/SplitIntoSubArray.java
@@ -7,7 +7,7 @@ import java.util.*;
 class SplitIntoSubArray {
 	public static void main(String[] args) {
 		int[] original = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};//, 10};
-		int splitSize = 3;
+		int splitSize = 6;
 		List<int[]> list = splitArray(original, splitSize);
 		list.forEach(splitArray -> System.out.println(Arrays.toString(splitArray)));
 	}
@@ -19,16 +19,12 @@ class SplitIntoSubArray {
 			return result;
 		}
 		int s = 0;
-		int[] temp = null;
+		int[] temp = new int[splitSize];
 		for (int i=0; i<array.length; i++) {
-			int min = Math.min((array.length - i), splitSize);
 			if (s == splitSize) {
 				result.add(temp);
-				temp = new int[min];
+				temp = new int[Math.min((array.length - i), splitSize)];
 				s = 0;
-			} else {
-				if (temp == null)
-					temp = new int[min];
 			}
 			temp[s++] = array[i];
 		}
