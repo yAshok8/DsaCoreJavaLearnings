8574034ca7b726ab12a55b1d5401c213035d0d2c
Ashok <yashok42@gmail.com>
2022-11-13 22:28:09 +0530

    removed un necessary commented code

--- src/main/java/DSA/arrays/gen/SplitIntoSubArray.java
+++ src/main/java/DSA/arrays/gen/SplitIntoSubArray.java
@@ -7,16 +7,7 @@ import java.util.*;
 class SplitIntoSubArray {
 	public static void main(String[] args) {
 		int[] original = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};//, 10};
-//		int[] original = {1, 2, 3, 4};
 		int splitSize = 3;
-		
-		/* expected Output 
-		[0, 1, 2]
-		[3, 4, 5]
-		[6, 7, 8]
-		[9]
-		*/
-
 		List<int[]> list = splitArray(original, splitSize);
 		list.forEach(splitArray -> System.out.println(Arrays.toString(splitArray)));
 	}
