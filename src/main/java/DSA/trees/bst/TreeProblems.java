d1275f105e4c8eba4737c3405dc719736b9d85ce
Ashok Yadav <yashok42@gmail.com>
2022-12-26 20:53:39 +0530

    check if tree is balanced binary search tree with less optimized o(n^2) approach

---
+++ src/main/java/DSA/trees/bst/TreeProblems.java
@@ -0,0 +1,15 @@
+package DSA.trees.bst;
+
+public class TreeProblems {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(3);
+        root.left.left.left = new TreeNode(4);
+        root.left.left.right = new TreeNode(4);
+        System.out.println(TreeOperations.isBalanced(root));
+    }
+
+}
