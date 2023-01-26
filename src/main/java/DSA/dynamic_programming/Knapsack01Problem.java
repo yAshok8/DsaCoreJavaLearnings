d8db33532669f029de9389c4591e889d7f6b33f9
Ashok Yadav <yashok42@gmail.com>
2023-01-27 00:16:21 +0530

    Unfinished Knapsack dynamic programming code

--- src/main/java/DSA/dynamic_programming/Knapsack01Problem.java
+++ src/main/java/DSA/dynamic_programming/Knapsack01Problem.java
@@ -3,6 +3,8 @@ package DSA.dynamic_programming;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class Knapsack01Problem {
     public static void main(String[] args) throws IOException {
@@ -21,17 +23,38 @@ public class Knapsack01Problem {
         }
 //        int [] wt = {1, 2, 3, 2, 2};
 //        int [] val = {8, 4, 0, 5, 3};
-        System.out.println(knapSackSimple(wt, val, n - 1, W));
+        int [] dp = new int[wt.length];
+        Arrays.fill(dp, -1);
+//        System.out.println(knapSackSimple(wt, val, 4, 4));
+        System.out.println(knapsackDP(wt, val, n - 1, W, dp));
+//        Arrays.stream(dp).forEach(System.out::println);
+//        System.out.println(knapSackSimple(wt, val, n - 1, W));
     }
 
     private static int knapSackSimple(int [] w, int [] v, int n, int W) {
-        if (W == 0 || n == -1) {
+        if (W == 0 || n == -1)
             return 0;
+        int ans = 0;
+        if (w[n] <= W)
+            ans =  Math.max(knapSackSimple(w, v, n - 1, W - w[n]) + v[n], knapSackSimple(w, v, n - 1, W));
+        else
+            ans = knapSackSimple(w, v, n - 1, W);
+        return ans;
+    }
+
+    private static int knapsackDP(int [] w, int [] v, int n, int W, int [] dp) {
+        if (W == 0 || n == -1)
+            return 0;
+        if (dp[n] == -1) {
+            if (w[n] <= W) {
+                int nonIncluding = knapsackDP(w, v, n - 1, W, dp);
+                int including = knapsackDP(w, v, n - 1, W - w[n], dp) + v[n];
+                dp[n] = Math.max(including, nonIncluding);
+            } else {
+                dp[n] = knapsackDP(w, v, n - 1, W, dp);
+            }
         }
-        if (w[n] < W) {
-            return Math.max(knapSackSimple(w, v, n - 1, W - w[n]) + v[n], knapSackSimple(w, v, n - 1, W));
-        }
-        return knapSackSimple(w, v, n - 1, W);
+        return dp[n];
     }
 
 }
