a80cdf9ba01f2a46d6fbb08df193f81fd49aed80
Ashok <yashok42@gmail.com>
2023-01-29 12:46:09 +0530

    Leetcode 768 final code for Max Chunks To Get the Array Sorted problem

--- src/main/java/leetcode/MaxChunksToSorted768.java
+++ src/main/java/leetcode/MaxChunksToSorted768.java
@@ -3,27 +3,20 @@ package leetcode;
 public class MaxChunksToSorted768 {
     public static void main(String[] args) {
         MaxChunksToSorted768 obj = new MaxChunksToSorted768();
-        System.out.println(obj.maxChunksToSorted(new int[]{2,1,3,4,4}));
+        System.out.println(obj.maxChunksToSorted(new int[]{2,4,1,6,5,9,7}));
     }
 
     public int maxChunksToSorted(int [] arr) {
-        int [] leftMax = new int[arr.length];
         int [] rightMin = new int[arr.length];
+        rightMin[rightMin.length - 1] = arr[arr.length - 1];
+        for (int i=arr.length - 2; i>=0; i--) {
+            rightMin[i] = Math.min(arr[i], rightMin[i+1]);
+        }
+        int ans = 1;
         int max = arr[0];
-        leftMax[0] = max;
-        int min = arr[arr.length - 1];
-        rightMin[arr.length - 1] = min;
-        for (int i=1; i<arr.length; i++) {
+        for (int i=0; i<rightMin.length - 1; i++) {
             max = Math.max(max, arr[i]);
-            leftMax[i] = max;
-        }
-        for (int i=arr.length - 1; i>=0; i--) {
-            min = Math.min(min, arr[i]);
-            rightMin[i] = min;
-        }
-        int ans = 0;
-        for (int i=1; i<rightMin.length; i++) {
-            if (leftMax[i-1] < rightMin[i]) {
+            if (max <= rightMin[i+1]) {
                 ans++;
             }
         }
