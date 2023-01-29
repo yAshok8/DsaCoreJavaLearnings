5d2da6d30e2e41ba9c5a71981b8117490d2bd3ed
Ashok <yashok42@gmail.com>
2023-01-29 13:20:38 +0530

    CompletableFuture thenApply() method

--- src/main/java/core_java_only/java8/CompletionStageDemo.java
+++ src/main/java/core_java_only/java8/CompletionStageDemo.java
@@ -7,57 +7,36 @@ public class CompletionStageDemo {
         CompletableFuture.supplyAsync(CompletionStageDemo::getOrder)
                 .thenApply(CompletionStageDemo::processOrder)
                 .thenApply(CompletionStageDemo::processPayment)
-                .thenApply(CompletionStageDemo::processPayment)
+                .thenApply(CompletionStageDemo::dispatch)
                 .thenAccept(CompletionStageDemo::sendEmail);
     }
 
     public static Order getOrder() {
         System.out.println("getOrder() started.");
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         System.out.println("getOrder() completed.");
         return new Order();
     }
 
     public static Order processOrder(Order order) {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        System.out.println("processOrder() started.");
         System.out.println("processOrder() completed.");
         return new Order();
     }
 
     public static Order processPayment(Order order) {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        System.out.println("processPayment() started.");
         System.out.println("processPayment() completed.");
         return new Order();
     }
 
     public static Order dispatch(Order order) {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        System.out.println("dispatch() started.");
         System.out.println("dispatch() completed.");
         return new Order();
     }
 
     public static Order sendEmail(Order order) {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        System.out.println("sendEmail() started.");
         System.out.println("sendEmail() completed.");
         return new Order();
     }
