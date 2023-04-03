b4bcc5b18fb891d77282395ff482dc22ba178701
Ashok Yadav <yashok42@gmail.com>
2023-04-03 11:33:45 +0530

    Added could not answered questions in 2023 job hunt

---
+++ src/main/java/interviewAsked/LYBL_unfinished.java
@@ -0,0 +1,9 @@
+package interviewAsked;
+
+/**
+ * Find the smallest positive integer num which is either not present in the array
+ * or
+ * cant be represented as a sum.
+ */
+public class LYBL_unfinished {
+}
