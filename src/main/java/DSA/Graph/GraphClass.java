83631b6278862db9d6d590868130163296c8c9a8
Ashok <44674369+yAshok8@users.noreply.github.com>
2023-08-22 10:41:54 +0530

    Create GraphClass.java

---
+++ src/main/java/DSA/Graph/GraphClass.java
@@ -0,0 +1,33 @@
+package DS;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GraphClass implements Graph {
+
+    private Map<Integer, List<Integer>> adjacencyList = null;
+
+    public GraphClass() {
+        adjacencyList = new HashMap<>();
+    }
+    @Override
+    public void addVertex(Integer vertex) {
+        adjacencyList.put(vertex, new ArrayList<>());
+    }
+
+    @Override
+    public void addEdge(Integer source, Integer destination) {
+        adjacencyList.get(source).add(destination);
+        adjacencyList.get(destination).add(source);
+    }
+
+    public Map<Integer, List<Integer>> getAdjacencyList() {
+        return adjacencyList;
+    }
+
+    public void printGraph() {
+        System.out.println(adjacencyList);
+    }
+}
