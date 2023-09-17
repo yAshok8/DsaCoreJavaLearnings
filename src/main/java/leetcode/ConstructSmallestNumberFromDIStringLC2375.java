e59316546c86f90058907ead04c5600938164b1c
Ashok <yashok42@gmail.com>
2023-09-18 00:26:55 +0530

    Wrote DI's code recursive solution (better than previous one)

--- src/main/java/leetcode/ConstructSmallestNumberFromDIStringLC2375.java
+++ src/main/java/leetcode/ConstructSmallestNumberFromDIStringLC2375.java
@@ -1,15 +1,66 @@
 package leetcode;
 
+import java.util.HashSet;
+
 @FunctionalInterface
 interface FunctionalCharCheck {
     boolean checkPattern(String pattern, int index, int [] num);
 }
 
 class ConstructSmallestNumberFromDIStringLC2375 {
+
+    String ans = null;
+
     public static void main(String[] args) {
         ConstructSmallestNumberFromDIStringLC2375 obj = new ConstructSmallestNumberFromDIStringLC2375();
-        System.out.println(obj.smallestNumber("DDD"));
+        System.out.println(obj.smallestNumber("ID"));
+        obj.smallestNumberRecursiveDFS("ID");
+        System.out.println();
         System.out.println(obj.smallestNumber("IIIDIDDD"));
+        obj.smallestNumberRecursiveDFS("IIIDIDDD");
+        System.out.println();
+        System.out.println(obj.smallestNumber("IDID"));
+        obj.smallestNumberRecursiveDFS("IDID");
+        System.out.println();
+        System.out.println(obj.smallestNumber("DIDDIDI"));
+        obj.smallestNumberRecursiveDFS("DIDDIDI");
+
+//        System.out.println(obj.ans);
+    }
+
+    private String smallestNumberRecursiveDFS(String pattern) {
+        int [] nums = new int[pattern.length() + 1];
+        dfs(nums, new HashSet<>(), 0, pattern);
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int num: nums) {
+            stringBuilder.append(num);
+        }
+        return stringBuilder.toString();
+    }
+
+    private boolean dfs(int[] nums, HashSet<Object> set, int index, String pattern) {
+        if (index == pattern.length() + 1) {
+            return true;
+        }
+        boolean res = false;
+        for (int i=1; i<=pattern.length() + 1; i++) {
+            if (!set.contains(i)) {
+                if (index > 0
+                        && ((pattern.charAt(index-1) == 'I' && nums[index-1] > i)
+                        || (pattern.charAt(index-1) == 'D' && nums[index-1] < i))) {
+                    return false;
+                }
+                nums[index] = i;
+                set.add(i);
+                res = dfs(nums, set, index + 1, pattern);
+                if (res) {
+                    break;
+                }
+                nums[index] = 0;
+                set.remove(i);
+            }
+        }
+        return res;
     }
 
     public String smallestNumber(String pattern) {
